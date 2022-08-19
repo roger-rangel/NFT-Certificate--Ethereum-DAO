518f0ebf74afa563dead8bfe6f85f7b6544e7817
Roger Rangel <roger.rangel.ee@gmail.com>
2022-08-19 16:27:34 +0200

    Pseudocode of dao smart contract

---
+++ contracts/DAO.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+contract DAOFactory {
+    // store deployed proposals
+
+    // create a Proposal
+
+    // view Proposals
+}
+
+contract DAO {
+    // Proposal structure
+
+    // Voter structure
+
+    // proposalCreator, minimumVotesRequired, voters, proposals
+
+    // constructor (proposalCreator, voter)
+
+    // get Proposal Name
+
+    // get total Number of Votes 
+
+    // set Votes to Proposal
+
+    // set count Votes
+
+}
